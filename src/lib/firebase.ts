3c9a319743ea9347e93d4e05508cb739affc56f0
Carlos Daniel Pasache Sánchez <carlospasachesanchez@gmail.com>
2026-06-24 01:54:27 -0500

    Configurar firebase usando variables de entorno (.env)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlCO0iqbUmTrxRmIzPrNW_osrvq5vXF8A",
-  authDomain: "kowalski-pwa.firebaseapp.com",
-  projectId: "kowalski-pwa",
-  storageBucket: "kowalski-pwa.firebasestorage.app",
-  messagingSenderId: "904750302469",
-  appId: "1:904750302469:web:1eb72e42b78a8f045c236c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
