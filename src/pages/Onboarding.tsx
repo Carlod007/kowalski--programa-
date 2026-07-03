6651b3c15a383b3e3ecfc77b081cd49a412ae42a
Carlos Daniel Pasache Sánchez <carlospasachesanchez@gmail.com>
2026-07-03 03:01:40 -0500

    Salir del onboarding

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,3 +1,15 @@
+import { signOut } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+
 export default function Onboarding() {
-  return <div>Onboarding</div>;
+  async function handleSignOut() {
+    await signOut(auth);
+  }
+
+  return (
+    <div>
+      <p>Onboarding</p>
+      <button onClick={handleSignOut}>Salir</button>
+    </div>
+  );
 }
